fbf42e214880cb8e911d8672489b9e92973614f6
Andrew Wells <andrew@wellsie.net>
2013-11-27 16:42:59 -0600

    Switched mirror to precise-updates to fix install pause bug
    http://askubuntu.com/questions/327825/long-pause-in-pxe-preseed-install

--- ubuntu_deploy.sh
+++ ubuntu_deploy.sh
@@ -8,8 +8,8 @@ rm -f $BASE_DIR/$1.img >& /dev/null
 
 
 virt-install -n $1 -r 2048 \
---vcpus=4 --vnc --os-type=linux --os-variant=virtio26 \
+--vcpus=2 --vnc --os-type=linux --os-variant=virtio26 \
 --disk path=$BASE_DIR/$1.img,size=12  \
--l http://s10-deploy/mirrors/ubuntu/dists/precise/main/installer-amd64/ \
+-l http://s10-deploy/mirrors/ubuntu/dists/precise-updates/main/installer-amd64/ \
 -x "netcfg/get_hostname=$1 locale=en_US preseed/locale=en_US keyboard-configuration/layoutcode=en_US console-setup/ask_detect=false netcfg/choose_interface=eth0 url=http://s10-deploy/ai/ubuntu/12.04/def.php?autopart=vda&desktop=0" >& ~/$1.log &
 disown
